d43bfdcf5bab301bf16212529da171fa8c7a9cca
Vanilla <osu_Vanilla@126.com>
2022-01-29 15:55:46 +0800

    feat[chromiumSync]: Enable Chromium Sync by setting client id and secret.

---
+++ chromiumSync.nix
@@ -0,0 +1,7 @@
+{ ... }:
+{
+  systemd.user.sessionVariables = {
+    GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
+    GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
+  };
+}
