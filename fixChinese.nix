35cdd99b6e46a7fc29754c776aeff88006dda708
Vanilla <osu_Vanilla@126.com>
2022-01-29 21:52:34 +0800

    feat[fixChinese]: Enable noto-fonts-cjk-*, adjust SC first.

---
+++ fixChinese.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+{
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+  ];
+
+  home.file.".fonts.conf".text = ''
+    <?xml version="1.0"?>
+    <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+    <fontconfig>
+      <alias>
+        <family>sans-serif</family>
+        <prefer>
+          <family>Noto Sans CJK SC</family>
+          <family>Noto Sans CJK TC</family>
+          <family>Noto Sans CJK JP</family>
+        </prefer>
+      </alias>
+      <alias>
+        <family>monospace</family>
+        <prefer>
+          <family>Noto Sans Mono CJK SC</family>
+          <family>Noto Sans Mono CJK TC</family>
+          <family>Noto Sans Mono CJK JP</family>
+        </prefer>
+      </alias>
+    </fontconfig>
+  '';
+}
