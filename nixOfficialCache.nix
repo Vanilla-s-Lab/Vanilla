09fb2c7e2ededed5f4a904381755ec9d67e5584c
Vanilla <osu_Vanilla@126.com>
2022-01-30 14:10:44 +0800

    feat[nixOfficialCache]: Adding NixOS official cache and keys.

---
+++ nixOfficialCache.nix
@@ -0,0 +1,6 @@
+{ ... }:
+{
+  nix.settings.substituters = [ "https://cache.nixos.org/" ];
+  nix.settings.trusted-public-keys =
+    [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+}
