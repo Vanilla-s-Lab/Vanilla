9ee392e5b87b4a5b7bdb248428f48e5ec2e654f9
Vanilla <osu_Vanilla@126.com>
2022-01-31 21:29:04 +0800

    feat[gnomeEnableNumLock]: Enable NumLock on GNOME with booting.

---
+++ gnomeEnableNumLock.nix
@@ -0,0 +1,7 @@
+{ ... }:
+{
+  dconf.settings = {
+    # https://wiki.archlinux.org/title/Activating_numlock_on_bootup#GNOME
+    "org.gnome.desktop.peripherals.keyboard".numlock-state = true;
+  };
+}
