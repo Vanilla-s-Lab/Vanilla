9095083e7b033bee4888c525bbc4a36617d4ff24
Vanilla <osu_Vanilla@126.com>
2022-01-31 23:20:15 +0800

    fix[gnomeEnableNumLock]: Hotfix NumLock dconf format, dor to slash.

--- gnomeEnableNumLock.nix
+++ gnomeEnableNumLock.nix
@@ -2,6 +2,6 @@
 {
   dconf.settings = {
     # https://wiki.archlinux.org/title/Activating_numlock_on_bootup#GNOME
-    "org.gnome.desktop.peripherals.keyboard".numlock-state = true;
+    "org/gnome/desktop/peripherals/keyboard".numlock-state = true;
   };
 }
